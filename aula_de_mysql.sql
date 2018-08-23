4a0b950846939c3d4b525bfbc2fc174ef15e3689
jaimerson <jaimerson_correia@hotmail.com>
2018-08-23 18:11:27 -0400

    Manipulando Linhas(UPDATE, DELETE E TRUNCATE)

--- aula_de_mysql.sql
+++ aula_de_mysql.sql
@@ -103,6 +103,19 @@ INSERT INTO cursos VALUES
 ('9','Cozinha Árabe','Aprenda a fazer Kibe','40','30','2018'),
 ('10','YouTuber','Gerar polêmica e ganhar inscritos','5','2','2018');
 
+-- Inserindo dados na tabela cursos(certos)
+INSERT INTO cursos VALUES
+('1','HTML5','Curso de HTML5','40','37','2014'),
+('2','Algoritmos','Lógica de Programação','20','15','2014'),
+('3','Photoshop','Dicas de Photoshop CC','10','8','2014'),
+('4','PHP','Curso de PHP para Iniciantes','40','20','2015'),
+('5','Java','Introduçao à Linguagem Java','40','29','2015'),
+('6','MySQL','Banco de Dados MySQL','30','15','2016'),
+('7','Word','Curso completo de Word','40','30','2016'),
+('8','Sapateando','Danças Rítmicas','40','30','2018'),
+('9','Cozinha Árabe','Aprenda a fazer Kibe','40','30','2018'),
+('10','YouTuber','Gerar polêmica e ganhar inscritos','5','2','2018');
+
 UPDATE cursos
 SET nome = 'HTML5'
 WHERE idcurso ='1';
@@ -116,6 +129,23 @@ SET nome = 'Java', carga = '40', ano = '2015'
 WHERE idcurso ='5'
 LIMIT 1;
 
+UPDATE cursos
+SET ano = '2050', carga = '800'
+WHERE ano = '2018';
+
+UPDATE cursos 
+SET ano = '2019', carga = '0' 
+WHERE ano = '2050' LIMIT 1;
+
+DELETE FROM cursos
+WHERE idcurso = '8';
+
+DELETE FROM cursos
+WHERE ano = '2050'
+LIMIT 2;
+
+-- Apagando todos os registros da tabela
+TRUNCATE TABLE cursos;
 
 --DDL	-> Data Definition Language
 --	CREATE DATABASE
@@ -125,7 +155,9 @@ LIMIT 1;
 
 --DML	-> Data Manipulation Language
 --		INSERT INTO
---
+--		UPDATE
+--		DELETE
+--		TRUNCATE
 
 
 
